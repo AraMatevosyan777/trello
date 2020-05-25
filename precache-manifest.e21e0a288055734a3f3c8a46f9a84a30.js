fd73d5c7639c85165b494683b633d2678e33b716
Aro Matevosyan <AroMatevosyan777@gmil.com>
2020-05-25 21:13:33 +0400

    Updates

---
+++ precache-manifest.e21e0a288055734a3f3c8a46f9a84a30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d861219010bf84866ff05a2ab1050907",
+    "url": "/trello/index.html"
+  },
+  {
+    "revision": "3ebc500e5029bc8d9dfe",
+    "url": "/trello/static/css/main.d621e7b6.chunk.css"
+  },
+  {
+    "revision": "128fd177c2cc7d3705d0",
+    "url": "/trello/static/js/2.a38b06cf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/trello/static/js/2.a38b06cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ebc500e5029bc8d9dfe",
+    "url": "/trello/static/js/main.1aa038b9.chunk.js"
+  },
+  {
+    "revision": "8a91e9a8166c047e5dff",
+    "url": "/trello/static/js/runtime-main.9268136e.js"
+  },
+  {
+    "revision": "3485d82e6b8e9477826790458062a362",
+    "url": "/trello/static/media/check.3485d82e.svg"
+  },
+  {
+    "revision": "f214aca276b77e6d97584bce571faab1",
+    "url": "/trello/static/media/home.f214aca2.svg"
+  }
+]);
